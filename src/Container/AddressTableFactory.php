3f0a026e8e8f3a86b6db8490f6c4faf3c7103612
Roland Rusch <roland@rusch.lu>
2023-09-12 17:35:39 +0200

    #4 Change License to AGPL-3.0-only

--- src/Container/AddressTableFactory.php
+++ src/Container/AddressTableFactory.php
@@ -1,12 +1,16 @@
 <?php
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 declare(strict_types=1);
 
 namespace Ruga\Contact\Container;
 
 use Psr\Container\ContainerInterface;
-use Ruga\Contact\Address\AddressTable;
 use Ruga\Contact\Subtype\Address\AbstractAddressTable;
+use Ruga\Contact\Subtype\Address\AddressTable;
 use Ruga\Db\Adapter\Adapter;
 
 class AddressTableFactory
